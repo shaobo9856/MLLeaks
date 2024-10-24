e757b9e74e81c27af56a6e7b503d1949cb9e0f9d
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-10-24 13:16:14 +0200

    add channel options for ImprovedShadowModel

--- model.py
+++ model.py
@@ -7,7 +7,9 @@ import torch.nn.functional as F
 class ImprovedShadowModel(nn.Module):
     def __init__(self, num_classes, is_cifar=True):
         super(ImprovedShadowModel, self).__init__()
+        channel = 3 if is_cifar else 1
         self.resnet = models.resnet50(pretrained=True)
+        self.resnet.conv1 = nn.Conv2d(channel, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
     def forward(self, x):
